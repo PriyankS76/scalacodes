7582e56ee53d36c2160af59e9df14b9e0d8ee2a8
sneha <priyank.shekhar90@gmail.com>
2024-08-15 13:07:43 +0530

    23rd Question

--- src/main/scala/Twentythree.scala
+++ src/main/scala/Twentythree.scala
@@ -1,8 +1,8 @@
 object Twentythree {
   def main(args:Array[String]):Unit={
-    var p = 180 ;
-    var c = p>200;
-    print("eligible for discount:")
+    var p = 180;
+    var loyalty_card = "True"
+    var c = (p>200 || loyalty_card == "True");
     print(c)
   }
 }
