728ab200eda6f1f4df81eb53cc11595e614cf5be
sneha <priyank.shekhar90@gmail.com>
2024-08-15 12:33:00 +0530

    15th Question

---
+++ src/main/scala/Fifteenth.scala
@@ -0,0 +1,11 @@
+object Fifteenth {
+  def main(args:Array[String]):Unit= {
+    var age = 15;
+    if(age<13)
+      print("Child")
+     if((age >= 13 && age <= 19))
+       print("teenager")
+     else
+       print("adult")
+      }
+}
