bae32c71444a02b0217f240003a09e3ed91d4a0d
sneha <priyank.shekhar90@gmail.com>
2024-08-15 13:15:43 +0530

    28th Question

---
+++ src/main/scala/Twenty8.scala
@@ -0,0 +1,8 @@
+object Twenty8 {
+  def main(args:Array[String]):Unit={
+    var price = 120 ;
+    var c = price>150 || price>100 ;
+    print("eligible for discount or free shipping:")
+    print(c)
+  }
+}
