11540b372711f0a9fa4d68b177107a089e17d792
sneha <priyank.shekhar90@gmail.com>
2024-08-15 12:17:04 +0530

    12th Question

---
+++ src/main/scala/Twelth.scala
@@ -0,0 +1,8 @@
+object Twelth {
+  def main(args:Array[String]):Unit={
+    var price = 120 ;
+    var c = price>150 || price>100 ;
+    print("eligible for discount or free shipping:")
+    print(c)
+  }
+}
