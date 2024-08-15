5d6a75c8c86c167867e703c30e9ca4549351b49a
sneha <priyank.shekhar90@gmail.com>
2024-08-15 12:38:18 +0530

    17th Question

---
+++ src/main/scala/Seventeenth.scala
@@ -0,0 +1,8 @@
+object Seventeenth {
+  def main(args:Array[String]):Unit={
+    var age = 70;
+    var c = age>60 && age<25 ;
+    print("Eligible for both senior citizen and student discount:")
+    print(c)
+  }
+}
