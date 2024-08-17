e0fcd214604634720bb16e8a9a388386be246f58
sneha <priyank.shekhar90@gmail.com>
2024-08-17 14:01:05 +0530

    Q3

---
+++ src/main/scala/Q3.scala
@@ -0,0 +1,16 @@
+object Q3 {
+  def main(args: Array[String]): Unit = {
+    var a = 10;
+    var b = 20;
+    var c = 30;
+    if (a > b && a > c) {
+      print("A is greatest")
+    }
+    else if (b > a && b > c) {
+      print("B is greatest")
+    }
+    else {
+      print("C is greatest")
+    }
+  }
+}
