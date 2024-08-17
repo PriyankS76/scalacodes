c75ea946469d6e1d35024083da58dd0ecb4c9736
sneha <priyank.shekhar90@gmail.com>
2024-08-17 14:04:03 +0530

    Q20

---
+++ src/main/scala/Q20.scala
@@ -0,0 +1,15 @@
+object Q20 {
+  def main(args: Array[String]): Unit = {
+    var a = "A";
+    var b = "B";
+    for (i <- 1 to 200) {
+      for(j<-1 to 1) {
+         print(a)
+        print(",")
+      }
+      print(b)
+      print(",")
+    }
+  }
+
+}
