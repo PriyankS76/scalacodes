25baa59e298d54f8e99a41c374861d832309f153
sneha <priyank.shekhar90@gmail.com>
2024-08-17 14:05:02 +0530

    Q27

---
+++ src/main/scala/Q27.scala
@@ -0,0 +1,11 @@
+object Q27 {
+  def main(args: Array[String]): Unit = {
+    var a = 1;
+    for (i <-1 to 24) {
+      if(i%3!=0)
+            print(i,"")
+      else if(i%3==0)
+      print("factor of three")
+    }
+  }
+}
