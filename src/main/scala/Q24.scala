b2b887ef2d995a904a33f0ae8f4387c0219f7a37
sneha <priyank.shekhar90@gmail.com>
2024-08-17 14:04:39 +0530

    Q24

---
+++ src/main/scala/Q24.scala
@@ -0,0 +1,14 @@
+object Q24 {
+  def main(args: Array[String]): Unit = {
+    var a = 5;
+    var b = 10;
+    for (i <- 1 to 7) {
+      for (j <- 1 to 1) {
+        print(a)
+        print(",")
+      }
+      print(b)
+      print(",")
+    }
+  }
+}
