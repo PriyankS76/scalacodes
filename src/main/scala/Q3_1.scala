d7b3cfba8925eeedd1b0ab5912fe3e8238b16011
sneha <priyank.shekhar90@gmail.com>
2024-08-21 10:20:53 +0530

    Assign 3_1

---
+++ src/main/scala/Q3_1.scala
@@ -0,0 +1,13 @@
+object Q3_1 {
+  def main(args: Array[String]): Unit = {
+
+    for (i <- 1 to 5) {
+      for (j <- 1 to i) {
+        print("*")
+        print(" ")
+
+      }
+      println()
+    }
+  }
+}
