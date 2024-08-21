7f40ecb96226037c718c77ef23b0ef231c78e7a0
sneha <priyank.shekhar90@gmail.com>
2024-08-21 10:28:21 +0530

    Assign 3_8

---
+++ src/main/scala/Q3_8.scala
@@ -0,0 +1,21 @@
+object Q3_8 {
+  def main(args: Array[String]): Unit = {
+
+    for (i <- 1 to 9) {
+      if (i % 2 != 0) {
+        for (j <- 1 to i) {
+          print("*")
+        }
+        println()
+      }
+    }
+    for (i <- 7 to 1 by -1) {
+      if (i % 2 != 0) {
+        for (j <- i to 1 by -1) {
+          print("*")
+        }
+        println()
+      }
+    }
+  }
+}
